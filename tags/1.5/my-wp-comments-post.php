d75b9a9273ad142cc1fcc74c7b9a00f19fd0f1ca
weiwei9@gmail.com <weiwei9@gmail.com@677eb94a-c430-0410-a6cc-0f8d9b1f61c0>
2008-02-26 04:14:36 +0000

    - CHG: Catch up with changes of Live Space - BUG: Escaped special chars for XML - NEW: Improved error logging when parsing error

---
+++ tags/1.5/my-wp-comments-post.php
@@ -0,0 +1,70 @@
+<?php
+require( dirname(__FILE__) . '/wp-config.php' );
+function my_wp_new_comment( $commentdata ) {
+    remove_filter('comment_flood_filter', 'wp_throttle_comment_flood');
+	$commentdata = apply_filters('preprocess_comment', $commentdata);
+	$commentdata['comment_post_ID'] = (int) $commentdata['comment_post_ID'];
+	$commentdata['user_ID']         = (int) $commentdata['user_ID'];
+
+	$commentdata['comment_author_IP'] = preg_replace( '/[^0-9., ]/', '',$_SERVER['REMOTE_ADDR'] );
+	$commentdata['comment_agent']     = $_SERVER['HTTP_USER_AGENT'];
+
+	$commentdata['comment_date']     = $commentdata['comment_date'];
+    $commentdata['comment_date_gmt']     = $commentdata['comment_date'];
+    
+	$commentdata = wp_filter_comment($commentdata);
+
+	$commentdata['comment_approved'] = wp_allow_comment($commentdata);
+
+	$comment_ID = wp_insert_comment($commentdata);
+
+	do_action('comment_post', $comment_ID, $commentdata['comment_approved']);
+
+	if ( 'spam' !== $commentdata['comment_approved'] ) { // If it's spam save it silently for later crunching
+		if ( '0' == $commentdata['comment_approved'] )
+			wp_notify_moderator($comment_ID);
+
+		$post = &get_post($commentdata['comment_post_ID']); // Don't notify if it's your own comment
+
+		if ( get_option('comments_notify') && $commentdata['comment_approved'] && $post->post_author != $commentdata['user_ID'] )
+			wp_notify_postauthor($comment_ID, $commentdata['comment_type']);
+	}
+    add_filter('comment_flood_filter', 'wp_throttle_comment_flood', 10, 3);
+	return $comment_ID;
+}
+nocache_headers();
+
+$comment_post_ID = (int) $_POST['comment_post_ID'];
+
+$status = $wpdb->get_row("SELECT post_status, comment_status FROM $wpdb->posts WHERE ID = '$comment_post_ID'");
+
+if ( empty($status->comment_status) ) {
+	do_action('comment_id_not_found', $comment_post_ID);
+	exit;
+} elseif ( 'closed' ==  $status->comment_status ) {
+	do_action('comment_closed', $comment_post_ID);
+	wp_die( __('Sorry, comments are closed for this item.') );
+} elseif ( 'draft' == $status->post_status ) {
+	do_action('comment_on_draft', $comment_post_ID);
+	exit;
+}
+
+$comment_author       = trim($_POST['author']);
+$comment_author_email = trim($_POST['email']);
+$comment_author_url   = trim($_POST['url']);
+$comment_content      = trim($_POST['comment']);
+$comment_date         = trim($_POST['date']);
+
+$comment_type = '';
+
+
+if ( '' == $comment_content )
+	wp_die( __('Error: please type a comment.') );
+
+$commentdata = compact('comment_post_ID', 'comment_author', 'comment_author_email', 'comment_author_url', 'comment_content','comment_date','comment_date_gmt', 'comment_type', 'user_ID');
+$comment_id = my_wp_new_comment( $commentdata );
+$comment = get_comment($comment_id);
+
+echo('Success');
+
+?>
